0dcaffaa20ef3f44fbb13de3943333cff2e42728
Adolligit <adelson.onod@gmail.com>
2021-09-13 23:31:09 -0300

    iniciando revisão sobre o bloco 5 - DOM

---
+++ fundamentos do desenvolvimento web/js-dom-eventos-web-storage/revisão de captulo/P1, Trybe.js
@@ -0,0 +1,12 @@
+// alterando o título
+document.getElementById("page-title").innerHTML = "Interestelar"
+
+// alterando o primeiro parágrafo
+document.getElementById("paragraph").innerHTML = "Excelente filme para quem curte ideais do espaço tempo."
+
+// alterando o segundo parágrafo
+document.getElementById("second-paragraph").innerText = "O começo do filme é um pouco morto mesmo, mas fica bom demais!"
+
+// alterando o subtítulo
+let subtitle = document.getElementById("subtitle");
+subtitle.innerHTML = "O início do filme: "

3905e81e7bb66456cf57e19d8a56671f640a01e5
Adolligit <adelson.onod@gmail.com>
2021-10-30 22:37:49 -0300

    reorganizando diretórios; iniciando exercícios de JEST

---
+++ course/fundamentos do desenvolvimento web/b7 - introdução á javascript es6 e testes unitários/3; primeiros passos em jest/p1/sum.test.js
@@ -0,0 +1,17 @@
+const { test } = require('@jest/globals');
+const sum = require('./sum');
+
+describe('Testando função sum', () => {
+
+  test('O retorno deve ser 9 quando função receber (4,5)', () => {
+    expect(9).toBe(sum(4,5));
+  });
+
+  test('O retorno deve ser 0 quando função receber (0,0)', () => {
+    expect(0).toBe(sum(0,0));
+  });
+
+  test('A função deve lançar um erro quando não receber valores numéricos', () => {
+    expect(() => {sum(4,"5")}).toThrow('parameters must be numbers')
+  });
+});

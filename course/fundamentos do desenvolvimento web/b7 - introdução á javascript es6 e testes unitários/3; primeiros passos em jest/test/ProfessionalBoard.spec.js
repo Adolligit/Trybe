e7404a8167e2402724c7c1199bba14891cf94e47
Adolligit <adelson.onod@gmail.com>
2021-11-17 15:30:18 -0300

    iniciando exercício bônus

---
+++ course/fundamentos do desenvolvimento web/b7 - introdução á javascript es6 e testes unitários/3; primeiros passos em jest/test/ProfessionalBoard.spec.js
@@ -0,0 +1,11 @@
+const professionalBoard = require("../src/ProfessionalBoard");
+
+describe("Título", () => {
+  test("professionalBoard é uma função", () => {
+    expect(typeof professionalBoard).toBe('function');
+  });
+  test("", () => {
+
+  });
+});
+

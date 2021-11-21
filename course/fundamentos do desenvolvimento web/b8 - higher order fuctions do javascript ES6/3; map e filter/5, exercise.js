9219050accfc329c6f57aeea8b2c4d1cb5068d63
Adolligit <adelson.onod@gmail.com>
2021-11-20 21:53:08 -0300

    exercise five completed; using FILTER with MAP and SORT

---
+++ course/fundamentos do desenvolvimento web/b8 - higher order fuctions do javascript ES6/3; map e filter/5, exercise.js
@@ -0,0 +1,15 @@
+/*
+ - Criar um vetor com o nome dos autores dos filmes do gênero de "Ficção Científica" e "Fantasia".
+ - Order: lexicográfica.
+*/
+
+const books = require('./books');
+
+function fantasyOrScienceFictionAuthors() {
+    return books
+    .filter(book => book.genre != "Terror")
+    .map(book => `${book.author['name']}`)
+    .sort();
+}
+
+console.log(fantasyOrScienceFictionAuthors());

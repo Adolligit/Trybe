39f4c0d992a4eb98ca46ee86539ba5fa5af8dd43
Adolligit <adelson.onod@gmail.com>
2021-12-22 16:12:32 -0300

    b3 e b4, exclusão de pastas e conteúdos

--- course/fundamentos do desenvolvimento web/b4 - introdução a javascript e lógica de programação/javascript - array loop for/p1.js
+++
@@ -1,8 +0,0 @@
-let myNameIs = ["Adelson", "Oliveira", "de", "Lima"];
-
-for(let i = 0; i < myNameIs.length; i++){
-    console.log(myNameIs[i])
-}
-console
-let tasksList = ["Tomar café", "Reunião", "Brincar com o cachorro"];
-console.log(tasksList.length);

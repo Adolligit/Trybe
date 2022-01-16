ae7ce4c52578ab0dcf778cd6ad28147a4e1f0d4c
Adolligit <adelson.onod@gmail.com>
2022-01-15 21:05:44 -0300

    primeiro componente React: lembre-se, nunca utilize palavras reservadas

---
+++ course/desenvolvimento front-end/d1_introducao/example-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Meliante from './DontUseWordReserved';
+
+function App() {
+  return (
+    <Meliante/>
+  );
+}
+
+export default App;
---
+++ course/desenvolvimento front-end/d1_introducao/example-app/src/DontUseWordReserved.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class DontUseWordReserved extends Component {
+  Meliante(){
+    return (
+      <>
+        <h1>Adelson</h1>
+        <p>i'm 23 old, my name's Adelson Lima and i develop software to web</p>
+      </>
+    );
+  }
+}
+
+export default DontUseWordReserved;

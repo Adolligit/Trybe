5861c1454722b52258720aef4237af86cf7c8c90
Adolligit <adelson.onod@gmail.com>
2022-01-29 20:15:46 -0300

    aprendendo a alterar o estado do componente

--- course/desenvolvimento front-end/b11 - componentes com estado, eventos e formulários com react/fancy-buttons/src/App.js
+++ course/desenvolvimento front-end/b11 - componentes com estado, eventos e formulários com react/fancy-buttons/src/App.js
@@ -4,25 +4,21 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
-    this.imprimeNaTela = this.imprimeNaTela.bind(this);
+    this.state = {
+      numberInterval: 0,
+    }
+    this.setInterval = this.setInterval.bind(this);
   }
 
-  imprimeNaTela(number){
-    switch(number){
-      case 1: console.log('Booom diaaaa!'); break;
-      case 2: console.log('Taaaarde!'); break;
-      case 3: console.log('Buenas noites'); break;
-      default: console.log('Numero inválido'); break;
-    }
+  setInterval() {
+    this.setState({
+      numberInterval: this.state.numberInterval + 1
+    });
   }
 
   render() {   
     return (
-      <>
-        <button onClick={ () => { this.imprimeNaTela(1) } } value="este é o valor">Primerio</button>
-        <button onClick={ () => { this.imprimeNaTela(2) }  }>Segundo</button>
-        <button onClick={ () => { this.imprimeNaTela(3) }  }>Terceiro</button>
-      </>
+        <button onClick={ this.setInterval } value="este é o valor">{this.state.numberInterval}</button>
     )
   }
 }

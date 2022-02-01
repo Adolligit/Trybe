e95d48f6dc40f9682bb17607c8cd333efb2bd734
Adolligit <adelson.onod@gmail.com>
2022-02-01 12:03:46 -0300

    Adicionando filtro pelo tipo do Pokemon

--- course/desenvolvimento front-end/b11 - componentes com estado, eventos e formulários com react/exercise-pokedex-state/src/Pokedex.js
+++ course/desenvolvimento front-end/b11 - componentes com estado, eventos e formulários com react/exercise-pokedex-state/src/Pokedex.js
@@ -4,43 +4,64 @@ import Pokemon from './Pokemon';
 class Pokedex extends React.Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
 			indexPokemon: 0,
+			powerfulAnimal: props.pokemons
 		};
-
+		
 		this.nextPokemon = this.nextPokemon.bind(this);
 		this.previousPokemon = this.previousPokemon.bind(this);
 	}
-
+	
 	nextPokemon() {
-		const { indexPokemon } = this.state;
+		const { indexPokemon, powerfulAnimal } = this.state;
 		const { pokemons } = this.props;
 
 		this.setState({
-			indexPokemon: (indexPokemon < pokemons.length - 1) ? indexPokemon + 1 : 0,
+			indexPokemon: (indexPokemon < powerfulAnimal.length - 1) ? indexPokemon + 1 : 0,
 		});
 	}
 
     previousPokemon() {
-		const { indexPokemon } = this.state;
-		const { pokemons: { length } } = this.props; 
+		const { indexPokemon, powerfulAnimal: { length } } = this.state;
 
 		this.setState({
 			indexPokemon: (indexPokemon > 0) ? indexPokemon - 1 : length - 1 
 		});
 	}
 
+	filtering(type){
+		const { pokemons } = this.props;
+		const filterType = pokemons.filter((pokemon) => pokemon.type === type);
+
+		this.setState({
+			indexPokemon: 0,
+			powerfulAnimal: filterType 
+		})
+		console.log(this.state.powerfulAnimal);
+	}
 
 	render() {
 		const { pokemons } = this.props;
-		const { indexPokemon } = this.state;
-
+		const { indexPokemon, powerfulAnimal } = this.state;
+	
 		return (
-			<div className="pokedex">
-				<button onClick={this.previousPokemon}>Anterior</button>
-				<Pokemon pokemon={pokemons[indexPokemon]} />
-				<button onClick={this.nextPokemon}>Próximo</button>
-  		</div>
+			<>
+				<div className="pokedex">
+					<button onClick={this.previousPokemon}>Anterior</button>
+					<Pokemon pokemon={ powerfulAnimal[indexPokemon] }/>
+					<button onClick={this.nextPokemon}>Próximo</button>
+				</div>
+				<div>
+					{ 
+						pokemons
+						.map(({ id, type }) => 
+							<button onClick={ () => this.filtering(type) } key={ id }> { type } </button>
+						) 
+					}
+				</div>
+			</>
 		);
 	}
 }

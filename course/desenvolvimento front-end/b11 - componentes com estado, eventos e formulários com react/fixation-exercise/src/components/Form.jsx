f815315326836f8107bab59ce7a2184651948601
Adolligit <adelson.onod@gmail.com>
2022-02-01 21:01:42 -0300

    adicionando e tratando o valor do checked

--- course/desenvolvimento front-end/b11 - componentes com estado, eventos e formulários com react/fixation-exercise/src/components/Form.jsx
+++ course/desenvolvimento front-end/b11 - componentes com estado, eventos e formulários com react/fixation-exercise/src/components/Form.jsx
@@ -9,28 +9,38 @@ class Form extends React.Component {
       first_name: "",
       last_name: "",
       description: "",
+      verificado: false,
     }
 
     this.capText = this.capText.bind(this);
   }
 
   capText({ target }){
-    const { value, name } = target;
+    const { value, name, type, checked } = target;
 
     this.setState({
-      [name]: value
+      [name]: (type !== "checkbox") ? value : checked
     });
   }
 
   render(){
+    const { selecao, first_name, last_name, description, value } = this.state; 
     return(
        <form action="">
-         <select onChange={this.capText} name="selecao" id="">
+         <br />
+         <select onChange={this.capText} name="selecao" value={ selecao }>
            <option value="item1">Só tem esse</option>
+           <option value="item2">mais um</option>
+           <option value="item3">último</option>
          </select>
-         <input onChange={this.capText} name="first_name" type="text" />
-         <input onChange={this.capText} name="last_name" type="text" />
-         <textarea onChange={this.capText} name="description" cols="30" rows="10" />
+         <br />
+         <input onChange={this.capText} name="first_name" type="text" value={ first_name }/>
+         <br />
+         <input onChange={this.capText} name="last_name" type="text" value={ last_name }/>
+         <br />
+         <textarea onChange={this.capText} name="description" cols="30" rows="10" value={ description }/>
+         <br />
+         <input onChange={this.capText} type="checkbox" name="verificado" value={ value } />
       </form>
     );
   }
